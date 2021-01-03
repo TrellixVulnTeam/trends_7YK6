b7a2de5502b55a783b2dc27a9098223745579711
Oluyemi <oluyemi.jegede@live.com>
2021-01-03 13:32:01 +0000

    added empty path

--- backend/django_app/mainapp/urls.py
+++ backend/django_app/mainapp/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', admin.site.urls),
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/', include('trends.urls')),
+    path('', include('mainapp.urls'))
 ]
