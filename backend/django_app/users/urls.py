fa4a79420e60c9c0537cc4a655998786ac4d9a91
Oluyemi <oluyemi.jegede@live.com>
2021-01-03 13:38:34 +0000

    added empty path

--- backend/django_app/users/urls.py
+++ backend/django_app/users/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from .views import APILoginView, APILogoutView, APIPasswordUpdateView
 
 urlpatterns = [
+    path('', APILoginView.as_view(), name='api_login'),
     path('login/', APILoginView.as_view(), name='api_login'),
     path('logout/', APILogoutView.as_view(), name='api_logout'),
     path('update_password/', APIPasswordUpdateView.as_view(), name='api_update_password')
